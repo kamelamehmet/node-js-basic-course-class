f53a4d01abe3ef567f1894db589a1567d8d7ee9d
Isa Turku <turkuisa00@gmail.com>
2024-06-25 15:56:23 +0200

    wip: db integration

--- basic-server/src/app.js
+++ basic-server/src/app.js
@@ -2,12 +2,13 @@ const fastify = require("fastify");
 const fastifySwagger = require("@fastify/swagger");
 const fastifySwaggerUI = require("@fastify/swagger-ui")
 const { bookRoutes } = require("./routes/v1/books");
-const jwt = require("@fastify/jwt");
+const postgres = require("@fastify/postgres")
 
 const build = (opts = {}, optsSwaggerUI = {}) => {
   const app = fastify(opts);
   app.register(fastifySwagger);
   app.register(fastifySwaggerUI, optsSwaggerUI)
+  app.register(fastify, { connectionString: process.env.CONNECTION_STRING })
   app.register(bookRoutes)
   return app;
 };
--- basic-server/src/schemas/v1/books.js
+++ basic-server/src/schemas/v1/books.js
@@ -3,7 +3,6 @@ const bookSchema = {
   type: 'object',
   required: ['title', 'author', 'isbn', 'publicationYear'],
   properties: {
-    id: { type: 'integer' },
     title: { type: 'string' },
     author: { type: 'string' },
     isbn: { type: 'string', pattern: '^[0-9]{10}$' },
@@ -15,7 +14,6 @@ const updateBookSchema = {
   type: 'object',
   required: ['title', 'author', 'publicationYear'],
   properties: {
-    id: { type: 'integer' },
     title: { type: 'string' },
     author: { type: 'string' },
     publicationYear: { type: 'integer', minimum: 1500, maximum: new Date().getFullYear() }
